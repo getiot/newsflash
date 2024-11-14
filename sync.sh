dba6a6e8786827d24c419b9eeb365f8f92fc12ea
luhuadong <luhuadong@163.com>
2024-11-14 23:11:52 +0800

    chore: auto get remote repo name

--- sync.sh
+++ sync.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-echo "==> GitHub"
-git push origin main
+current_date=$(date +'%Y%m%d')
 
-echo "==> Gitee"
-git push gitee main
+commit_message="docs: add ${current_date} news flash"
 
-echo "Done!"
+#git add .
+git commit -m "$commit_message"
+
+remotes=$(git remote -v | awk '{print $1}' | sort | uniq)
+main_branch="main"
+
+# 遍历所有远程仓库，推送 main 分支
+for remote in $remotes; do
+    echo "Pushing to $remote..."
+    git push $remote $main_branch
+done
+
+echo "Commit and push to all remotes completed."
+
